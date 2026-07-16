823b0573e19c0933531c1cdf63092f00e86138d9
Francesco <francescopolella2@gmail.com>
2026-07-16 16:58:08 +0200

    Update env.sh

--- env.sh
+++ env.sh
@@ -1,20 +1,39 @@
 #bigdft-libnegf environment --> source env.sh (before compiling bigdft)
+#unset previous variables
+unset C_INCLUDE_PATH
+unset CPLUS_INCLUDE_PATH
+unset CPATH
+unset CPPFLAGS
+unset LIBS
+unset LDFLAGS
+unset FCFLAGS
+unset CFLAGS
+unset CXXFLAGS
+unset LIBNEGF_INC
+unset LIBNEGF_LIB
+unset MPIFX_LIB
+unset LIBRARY_PATH
+unset LD_LIBRARY_PATH
+unset PKG_CONFIG_PATH
 
+#export variables
 export CC=$(which mpicc)
 export CXX=$(which mpicxx)
 export FC=$(which mpifort)
 export F77=$(which mpifort)
+export MPICC=$CC
+export MPIFC=$FC
 export LIBNEGF_INC=/home/francesco/bigdft-negf/pybigdfttest/libnegf/build/src/include
 export LIBNEGF_LIB=/home/francesco/bigdft-negf/pybigdfttest/libnegf/build/src
 export MPIFX_LIB=$CONDA_PREFIX/lib
 export CPPFLAGS="-I$LIBNEGF_INC"
-export CFLAGS="-I$LIBNEGF_INC"
-export CXXFLAGS="-I$LIBNEGF_INC"
-export FCFLAGS="-fopenmp -I$LIBNEGF_INC"
-export FFLAGS="-fopenmp -I$LIBNEGF_INC"
+export CFLAGS="-O2 -I$LIBNEGF_INC"
+export CXXFLAGS="-O2 -I$LIBNEGF_INC"
+export FCFLAGS="$FCFLAGS -O2 -fopenmp -fallow-argument-mismatch -I$LIBNEGF_INC"
+export FFLAGS="$FFLAGS -fopenmp -I$LIBNEGF_INC"
 export LDFLAGS="-fopenmp \
--L$LIBNEGF_LIB -lnegf \
--L$MPIFX_LIB -lmpifx"
+-L$MPIFX_LIB -lmpifx \
+-L$LIBNEGF_LIB -lnegf"
 export LIBRARY_PATH=$LIBNEGF_LIB:$MPIFX_LIB:$LIBRARY_PATH
 export LD_LIBRARY_PATH=$LIBNEGF_LIB:$MPIFX_LIB:$LD_LIBRARY_PATH
 export PKG_CONFIG_PATH=$LIBNEGF_LIB/pkgconfig:$PKG_CONFIG_PATH # if *.pc file doesn't exist nothing happen
